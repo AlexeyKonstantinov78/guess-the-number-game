81dbc036b23b9f9c74d2cfc5eeaf3fc47b668f5a
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-09 16:01:02 +0300

    задача 1 пункт 3 запуск с нуля

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -9,18 +9,34 @@ export class ClassComponent extends React.Component {
     randomNumber:
       Math.floor((Math.random() * this.props.max - this.props.min) +
       this.props.min),
-    buttonText: 'Угадать'
+    buttonText: 'Угадать',
+    count: 0,
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     this.setState(state => {
+      console.log(state);
+
+      if (state.buttonText === 'Сыграть ещё') {
+        return {
+          result: 'Результат',
+          userNumber: '',
+          randomNumber:
+            Math.floor((Math.random() * this.props.max - this.props.min) +
+            this.props.min),
+          buttonText: 'Угадать',
+          count: 0,
+        };
+      }
+
       if (!state.userNumber) return state;
 
       if (state.userNumber > state.randomNumber) {
         return {
           result: `${state.userNumber} больше загадоного`,
           userNumber: '',
+          count: state.count + 1,
         };
       }
 
@@ -28,11 +44,14 @@ export class ClassComponent extends React.Component {
         return {
           result: `${state.userNumber} меньше загадоного`,
           userNumber: '',
+          count: state.count + 1,
         };
       }
 
       return {
-        result: `Вы угадали, загаданное число ${state.userNumber}`,
+        result: `Вы угадали, загаданное число 
+          ${state.userNumber} 
+          c ${state.count + 1} попытки`,
         userNumber: '',
         buttonText: 'Сыграть ещё'
       };
